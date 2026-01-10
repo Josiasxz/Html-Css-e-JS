5067025189d22dd11f086ae455c76a3e9ca50f76
Josiasxz <josuemt112@gmail.com>
2026-01-10 19:35:15 -0300

    Ex de idade
    Tenho que acabar de fazer o exercicio de idade ainda

--- Atividades JS/Idade/script.js
+++ Atividades JS/Idade/script.js
@@ -1,3 +1,30 @@
-var age = document.querySelector('#age')
-var sex;
+function calcRes()
+{
+    var age = document.querySelector('#age')
 
+    //Elementos
+    var result = window.document.getElementById('res')  
+    var checkMas = window.document.getElementById('mas')
+    var checkFem = window.document.getElementById('fem')
+    var sex = 'indefinido'
+    
+    // Verificação de idade
+    if (age > 2026 || age < 1875)
+        result.innerHTML = `<strong>[ERRO]</strong> Ano ${age} invalido! `
+    
+    // Verificação de sexo
+    if (checkMas.checked)
+    {
+        sex = 'Masculino'
+    }
+    else if (checkFem.checked)
+    {
+        sex = 'Feminino'
+    }
+    else
+    {
+        result.innerHTML = '<strong>[ERRO]</strong> Não foi definido sexo!'
+    }
+
+    
+}
