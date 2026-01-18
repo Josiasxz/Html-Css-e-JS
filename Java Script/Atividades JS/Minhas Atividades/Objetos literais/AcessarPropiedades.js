0b48f4e87a9f7eae63e82ef5b014befa6d6841e0
Josiasxz <josuemt112@gmail.com>
2026-01-18 14:35:30 -0300

    Objetos literais
    Acessando propiedades dos objetos literais

---
+++ Java Script/Atividades JS/Minhas Atividades/Objetos literais/AcessarPropiedades.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    //Propiedades
+    nome: "Josiazsx", // Virgula para separar propiedades
+    idade: 16,
+
+    //Metodos
+    info: function(){ // Metodo
+        console.log(pessoa.nome)
+        console.log(pessoa.idade)
+    }
+}
+
+pessoa.info() // Chamada da função/ metodo do objeto "pessoa"
