7dd42ccaab6c1955d5d603c3cdf022ffae4fc9e0
Josiasxz <josuemt112@gmail.com>
2026-01-21 17:22:13 -0300

    ArrowFunction

---
+++ Java Script/Atividades JS/Minhas Atividades/This/ArrowFunction.js
@@ -0,0 +1,22 @@
+// Função inteira arrow function
+const sum = (a, b) => {
+    return a + b;
+}
+
+// Função resumida
+const som = (a,b) => a + b;
+
+// ------------------- Transformar função em arrow function ----------------------------
+
+// Função
+function isAdult(age)
+{
+    return age >= 18;
+}
+
+// Arow Function
+
+const isAdult2 = (age) => 
+{
+    age >= 18;
+}
