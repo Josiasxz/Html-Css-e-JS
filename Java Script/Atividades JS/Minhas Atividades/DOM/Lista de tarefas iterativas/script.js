b877bc25aa6c54f433fc63d440c66427570f9158
Josiasxz <josuemt112@gmail.com>
2026-02-09 18:22:11 -0300

    Lista de tarefas interativa

--- Java Script/Atividades JS/Minhas Atividades/DOM/Lista de tarefas iterativas/script.js
+++ Java Script/Atividades JS/Minhas Atividades/DOM/Lista de tarefas iterativas/script.js
@@ -1,4 +1,3 @@
-const { createElement } = require("react");
 
 const textIn = document.querySelector('#inputTarefa');
 const createBtn = document.querySelector('#createBtn');
@@ -7,24 +6,36 @@ const List = document.querySelector('.list');
 
 const Tasks = {
     
-    taskList: [],
+    taskList: [
+        {task: "Teste", conclude: true, }
+    ],
 
     createTask: function()
     {
-        this.taskList.push(textIn.value)
+        this.taskList.push(task: textIn.value, )
 
         return this.taskList.at(-1)
     }
+}
+
+function configureButton(button)
+{
+
 }
 
 function createTasks(newTask)
 {
     // Cria a div filha que vai comportar o elemento
     const newDiv = document.createElement('div')
+    newDiv.classList.add("group")
+    List.append(newDiv)
+
     // Cria a nova lista
     const newTask = document.createElement('li')
     List.classList.add("item")
+
     // Cria os novos botões
+    const newConcluirBtn = document.createElement('input')
 
     List.append(newTask);    
 }
